891475c2071d73e127e33e3090d221b28113f27a
RmanKartavcev <128297080+Ramzes8@users.noreply.github.com>
2023-04-16 18:48:51 +0900

    added shop

--- src/item.py
+++ src/item.py
@@ -8,7 +8,6 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
@@ -21,7 +20,6 @@ class Item:
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         self.all = self.price * self.quantity
@@ -32,4 +30,3 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
-
